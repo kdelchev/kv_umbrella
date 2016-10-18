f864d11af9912957bd5350362344c0447fff4b94
Konstantin Delchev <kdelchev@gmail.com>
2016-10-18 23:27:12 +0300

    Add not found handler

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -79,4 +79,8 @@ defmodule KVServer do
     :gen_tcp.send(socket, "ERROR\r\n")
     exit(error)
   end
+
+  defp write_line(socket, {:error, :not_found}) do
+    :gen_tcp.send(socket, "NOT FOUND\r\n")
+  end
 end
